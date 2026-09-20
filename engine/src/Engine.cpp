8d32823d0d202b37efac5fe677b78ff8b66cf61d
Psina69 <ktoto14102010@gmail.com>
2026-09-20 10:37:45 +0300

    set up workflow

---
+++ engine/src/Engine.cpp
@@ -0,0 +1,6 @@
+#include "vkgfx2/vkgfx2.h"
+#include <print>
+
+void Engine::test() {
+	std::printf("Hey, its working");
+}
---
+++ sandbox/src/main.cpp
@@ -0,0 +1,8 @@
+#include <vkgfx2/vkgfx2.h>
+
+int main() {
+	Engine test;
+	test.test();
+
+	return 0;
+}
